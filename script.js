b4a7acb27b64b54a27299fe312289d7648276aba
Theo <143622653+Speender@users.noreply.github.com>
2024-02-05 21:52:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const NOquestion = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "GOOD 🥰😘";
+  gif.src =
+    "https://gifdb.com/images/high/cat-kiss-love-hearts-fmy9xzekhcve1nen.gif";
+});
+
+noBtnBtn.addEventListener("click", () => {
+  question.innerHTML = "TF YOU MEAN NO? 🤨";
+  gif.src =
+    "https://media1.tenor.com/m/yNMGjXsoYGUAAAAd/cat-cats.gif";
+});
